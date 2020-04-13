fd49c1e351fb2382f0ee35fa9301bc73a3d254df
vlecommandeur <valentin.lecommandeur@gmail.com>
2020-04-13 17:08:11 +0200

    replace redux with context

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -4,15 +4,15 @@ import ColorContext from "../contexts/ColorContext";
 
 class Button extends React.Component {
   //static contextType = LanguageContext;
-  renderSubmit(value) {
-    return value === "english" ? "Submit" : "Voorleggen";
+  renderSubmit(language) {
+    return language === "english" ? "Submit" : "Voorleggen";
   }
 
   renderButton(color) {
     return (
       <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
-          {(value) => this.renderSubmit(value)}
+          {({ language }) => this.renderSubmit(language)}
         </LanguageContext.Consumer>
       </button>
     );
